19c3276bceaf5f734ca02e010489ede8b7b7a598
lyh221 <lyh221@naver.com>
2026-09-16 13:21:32 +0900

    Component 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import Greeting from "./Greeting";
+
 function App() {
+  //logical
+  //구조분해할당 - object destucturing
+  const animals = ["dog","cat","pig"];
+  // const second = animals[1];
+
+  const [ potato, second] = animals;
+
+  console.log("🚀 ~ App ~ potato:", potato)
+  console.log("🚀 ~ App ~ second:", second)
+  
+  const todoItem = {
+    todo: "React학습하기",
+    isComplete: false
+  }
+
+  const {todo, isComplete} = todoItem
+  console.log("🚀 ~ App ~ todo:", todo)
+
+  //view
   return (
     <div className="App">
       Hello World!
+      <Greeting />
     </div>
   );
 }
